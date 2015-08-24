7b7da6e105ca80f871f77cfdb9f44df8fceb128c
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-08-23 21:33:38 -0400

    updates with MCID stuff

--- lib/parser.py
+++ lib/parser.py
@@ -8,17 +8,18 @@ class Opinion:
   def __init__(self, filename):
     self.fname = filename
     self.pdf = PdfFileReader(filename)
-    self.opinion = []
     self.opinion = self.getPages()
 
   def getPages(self):
+    self.opinion = []
     for page in self.pdf.pages:
       self.opinion.append(page.getContents().getData().decode(encoding="utf-8",errors="strict"))
     return self.opinion
 
-  def extractMCIDs(txt, pattern):
-    out = re.compile(pattern).split(txt)
-    return out
+  def splitMCIDs(self):
+    pattern = "<</MCID \d+ >>"
+    self.MCIDs = re.compile(pattern).split(str(self))
+    return self.MCIDs
   
   def __str__(self):
     return ''.join(self.opinion)
@@ -27,3 +28,4 @@ if __name__ == "__main__":
   fname = 'test_opinion.pdf'
   o = Opinion(fname)
   print(o)
+  #print(o.isplitMCIDs())
